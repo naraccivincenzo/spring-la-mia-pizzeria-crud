f4779239ec9cdea00dc6103e5ce95013d524b1f1
naracci.vincenzo <143946955+naraccivincenzo@users.noreply.github.com>
2024-09-12 21:51:17 +0200

    Edit completed

--- src/main/java/org/lessons/booleaners/springlamiapizzeriacrud/controller/PizzaController.java
+++ src/main/java/org/lessons/booleaners/springlamiapizzeriacrud/controller/PizzaController.java
@@ -44,11 +44,26 @@ public class PizzaController {
 
     @PostMapping("/create")
     public String store(@Valid @ModelAttribute("pizza") Pizza formPizza, BindingResult bindingResult, Model model) {
-        if(bindingResult.hasErrors()) {
+        if (bindingResult.hasErrors()) {
             return "/pizzas/create";
-        } else {
-            repo.save(formPizza);
-            return "redirect:/pizzas";
         }
+        repo.save(formPizza);
+        return "redirect:/pizzas";
+    }
+
+    @GetMapping("/edit/{id}")
+    public String edit(@PathVariable("id") Integer id, Model model) {
+        model.addAttribute("pizza", repo.findById(id).get());
+        return "/pizzas/edit";
+    }
+
+    @PostMapping("/edit/{id}")
+    public String update(@Valid @ModelAttribute("pizza") Pizza formPizza, BindingResult bindingResult, Model model) {
+
+        if (bindingResult.hasErrors()) {
+            return "/pizzas/edit";
+        }
+        repo.save(formPizza);
+        return "redirect:/pizzas";
     }
 }
